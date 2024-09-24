16304381f9e3e78ce632ceccac5a562a397dfbc5
dg1418 <dg1418>
2024-09-24 11:13:36 +0900

    typeORM설치 & entity.ts 에 model클래스 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModel } from './posts/entities/posts.entity';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      // 데이터베이스 타입
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [PostModel],
+      synchronize: true, //nest에서 작성하는 typeorm과 데이터베이스의 싱크를 맞출꺼냐? 묻는것
+      //개발단계에서는 true로 해도 배포할때는 반드시 false해야함
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/posts/entities/posts.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+//엔티티 데코레이터 사용시 클래스를 보고 테이블이 만들어짐
+@Entity()
+export class PostModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  author: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  likeCount: number;
+
+  @Column()
+  commentCount: number;
+}
